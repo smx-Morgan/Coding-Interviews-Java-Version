ad4e497be2923d2f1e2d1ca3da66c0bdfdf2fe56
smx_Moragn <1048492656@qq.com>
2023-03-01 08:47:45 +0800

    更新14题答案 动态规划版本

---
+++ src/Coding14.java
@@ -0,0 +1,31 @@
+public class Coding14 {
+    //剑指 Offer 14- I. 剪绳子
+    public int cuttingRope(int n) {
+        if(n == 2){
+            return 1;
+        }
+        if(n == 3){
+            return 2;
+        }
+        int[] products = new int[n + 1];
+        products[0] = 0;
+        products[1] = 1;
+        products[2] = 2;
+        products[3] = 3;
+        products[4] = 4;
+        //动态规划，找到每个product的最佳值，并输出
+        for(int i = 4 ; i <= n;++i){
+            int max = 0;
+            for(int j = 1; j <= i/2; ++j){
+                int temp = products[j]*products[i-j];
+                if(temp > max){
+                    products[i] = temp;
+                    max = temp;
+                }
+            }
+
+        }
+        return products[n];
+
+    }
+}
