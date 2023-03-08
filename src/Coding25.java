6d8f83df72c3a487edec4a02f2c416adac82e39d
smx_Moragn <1048492656@qq.com>
2023-03-08 11:20:11 +0800

    更新25题

---
+++ src/Coding25.java
@@ -0,0 +1,36 @@
+public class Coding25 {
+    //合并有序链表
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        ListNode head = new ListNode(0);
+        if(l1.val >= l2.val){
+            head.val = l2.val;
+        }else{
+            head.val = l1.val;
+        }
+        ListNode node;
+        node = head;
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                node.next = l1;
+                l1 = l1.next;
+            }else{
+                node.next = l2;
+                l2 = l2.next;
+            }
+            node = node.next;
+        }
+        if(l1 == null && l2 != null){
+            node.next = l2;
+        }
+        if(l2 == null && l1 != null){
+            node.next = l1;
+        }
+        return head.next;
+    }
+}
