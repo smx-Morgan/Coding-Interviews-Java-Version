91e8d062dfd97051f9dbc70c382a0a5aade60404
smx_Moragn <1048492656@qq.com>
2023-03-09 11:19:04 +0800

    更新26题

---
+++ src/Coding26.java
@@ -0,0 +1,30 @@
+public class Coding26 {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return dfs(A,B);
+    }
+    public boolean dfs(TreeNode A, TreeNode B){
+        boolean result = false;
+        if(A == null || B == null){
+            return false;
+        }
+        if(A.val == B.val){
+            result = checkSub(A,B);
+        }
+        if(!result){
+            result = dfs(A.left,B) || dfs(A.right,B);
+        }
+        return result;
+    }
+    public boolean checkSub(TreeNode A, TreeNode B){
+        if(B == null){
+            return true;
+        }
+        if(A == null){
+            return false;
+        }
+        if(A.val != B.val){
+            return false;
+        }
+        return checkSub(A.left,B.left) && checkSub(A.right,B.right);
+    }
+}
