1c9af5ab55a1e36c7b9add2ddcccf85c31e53da7
smx_Moragn <1048492656@qq.com>
2023-03-24 16:36:10 +0800

    更新36题

---
+++ src/Coding36.java
@@ -0,0 +1,30 @@
+import NodeFor36.Node;
+public class Coding36 {
+    //思路首先我们进行中序遍历，利用head和pre两个节点作为左右侧节点的记录
+    Node head, pre;
+    public Node treeToDoublyList(Node root) {
+        if(root==null) return null;
+        dfs(root);
+
+        pre.right = head;
+        head.left =pre;//进行头节点和尾节点的相互指向，这两句的顺序也是可以颠倒的
+
+        return head;
+
+    }
+
+    public void dfs(Node cur){
+        if(cur==null) return;
+        dfs(cur.left);
+
+        //pre用于记录双向链表中位于cur左侧的节点，即上一次迭代中的cur,当pre==null时，cur左侧没有节点,即此时cur为双向链表中的头节点
+        if(pre==null) head = cur;
+            //反之，pre!=null时，cur左侧存在节点pre，需要进行pre.right=cur的操作。
+        else pre.right = cur;
+
+        cur.left = pre;//pre是否为null对这句没有影响,且这句放在上面两句if else之前也是可以的。
+
+        pre = cur;//pre指向当前的cur
+        dfs(cur.right);//全部迭代完成后，pre指向双向链表中的尾节点
+    }
+}
