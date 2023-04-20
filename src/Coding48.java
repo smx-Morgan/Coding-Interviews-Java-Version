cb2b63ade1882412c865e7f60f0aa6ee2fd0efc5
smx_Moragn <1048492656@qq.com>
2023-04-20 09:55:32 +0800

    更新48

---
+++ src/Coding48.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Coding48 {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> dic = new HashMap<>();
+        int i = -1, res = 0;
+        for(int j = 0; j < s.length();j++){
+            if(dic.containsKey(s.charAt(j))){
+                i = Math.max(i,dic.get(s.charAt(j)));
+            }
+            dic.put(s.charAt(j),j);
+            res = Math.max(res,j-i);
+        }
+        return res;
+    }
+}
