8a04244a206a7a527e7144aa54c48eda2a9911c1
smx_Moragn <1048492656@qq.com>
2023-05-15 21:22:10 +0800

    更新59

---
+++ src/Coding59.java
@@ -0,0 +1,27 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Coding59 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums.length == 0 || k == 0) return new int[0];
+        Deque<Integer> deque = new LinkedList<>();
+        int[] res = new int[nums.length - k + 1];
+        for(int i = 0; i < k; i++) {
+            while(!deque.isEmpty() && deque.peekLast() < nums[i])
+                deque.removeLast();
+            deque.addLast(nums[i]);
+        }
+        res[0] = deque.peekFirst();
+        for(int i = k; i < nums.length;i++){
+            if(deque.peekFirst() == nums[i - k])
+                deque.removeFirst();
+            while(!deque.isEmpty() && deque.peekLast() < nums[i]){
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+            res[i - k + 1] = deque.peekFirst();
+        }
+        return res;
+
+    }
+}
